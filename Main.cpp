783097ad09782b76c7f7fb92c51da9c50999208e
Saimon <smazog@eafit.edu.co>
2025-02-19 20:30:34 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,15 @@
 //Aca va el codigo correspondiente a la actividad
+#include <iostream>
+
+using namespace std;
+
+void menu();
+
+int main(){
+    menu();
+    return 0;
+}
+
+void menu() {
+    cout << "Bienvenido al sistema de registro de estudiantes" << endl;
+}
