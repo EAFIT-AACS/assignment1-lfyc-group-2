3baf1888250041f228b149641c4dec31240a8a87
Saimon <smazog@eafit.edu.co>
2025-02-19 20:08:20 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -7,6 +7,5 @@ int main()
     cout << "Hello, World!" << endl;
     return 0;
 
-    cout << "Enotnces se supone que trabajao aqui y luego lo subo" << endl;
     cout << "Enotnces se supone que trabajo aqui y luego lo subo" << endl;
 }
