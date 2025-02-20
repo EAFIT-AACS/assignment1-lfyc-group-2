87098e7f39efd70a0c1bac3dbe49f9711e74af53
Saimon <smazog@eafit.edu.co>
2025-02-19 20:15:28 -0500

    preparacion

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,1 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    cout << "Hello, World!" << endl;
-    return 0;
-
-    cout << "Enotnces se supone que trabajo aqui y luego lo subo" << endl;
-    cout << "Que chevre" << endl;
-}
+//Aca va el codigo correspondiente a la actividad
