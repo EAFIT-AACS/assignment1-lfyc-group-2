dd0d7b5bf43969723e3dba52df27ad0e3f3c32ea
Saimon <smazog@eafit.edu.co>
2025-02-22 16:06:17 -0500

    adelanto de lectura de atrchivo

--- Main.cpp
+++ Main.cpp
@@ -3,10 +3,13 @@
 //Se incluyen las libreriás necesarias para la ejecución del programa
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
+#include <vector>
 using namespace std;
 
 void readFile();
+vector<string> alfabeto;
 
 int main(){
     readFile();
@@ -14,14 +17,28 @@ int main(){
 }
 
 void readFile(){
-    ifstream archivo("files.txt");
-    string line;
+    ifstream archivo("File.txt"); // Abrimos el archivo que contiene los DFA´s a minimizar
 
     if (!archivo) {
         cout << "Error al abrir el archivo, intentelo de nuevo" << endl;
         return;
     } 
+    
+    string line;
+    getline(archivo, line); // Leemos la primera linea del archivo
+    int c = stoi(line); 
+    archivo.ignore(); // Ignoramos la siguiente linea del archivo
 
-    cout << "Archivo abierto exitosaente" << endl;
+    for (int i= 0; i < c; i++) {
+        getline(archivo, line); // Leemos la siguiente linea del archivo que representa el numero de estados que tiene el DFA
+        int numEstados = stoi(line);
+
+        getline(archivo, line); // Leemos la siguiente linea del archivo que representa el alfabeto del DFA
+        stringstream ss(line);
+        string simbolo;
+        while (getline(ss, simbolo, ' ')) {  // Almacenamos los simbolos del alfabeto en un vector
+            alfabeto.push_back(simbolo);      
+        }
+    }
 };
 
